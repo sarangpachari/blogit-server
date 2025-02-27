13d61e67350ccc9fcdf7c770f1ca39452835e81e
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2025-02-27 12:35:40 +0530

    Post Edit completed

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -30,35 +30,77 @@ router.post(
 router.get("/dashboard", authMiddleware, async (req, res) => {
   try {
     const userId = req.user.id;
-    const userPosts = await Post.find({userId}).sort({createdAt: -1});
+    const userPosts = await Post.find({ userId }).sort({ createdAt: -1 });
 
-    res.status(200).json(userPosts)
+    res.status(200).json(userPosts);
   } catch (error) {
-    console.error(error)
+    console.error(error);
     res.status(500).json({ error: "Server error while fetching user posts" });
   }
 });
 
 //GET ALL POSTS
-router.get("/", async(req,res)=>{
+router.get("/", async (req, res) => {
   try {
-    const posts = await Post.find({ isPrivate: false }).sort({createdAt: -1})
-    res.status(200).json(posts)
+    const posts = await Post.find({ isPrivate: false }).sort({ createdAt: -1 });
+    res.status(200).json(posts);
   } catch (error) {
-    console.error(error)
+    console.error(error);
     res.status(500).json({ error: "Server error while fetching posts" });
   }
-})
+});
 
 //REMOVE POSTS
-router.delete("/:id/remove",authMiddleware, async(req,res)=>{
-  const {id} = req.params
+router.delete("/:id/remove", authMiddleware, async (req, res) => {
+  const { id } = req.params;
   try {
-    const removePost = await Post.findByIdAndDelete({_id:id})
-    res.status(200).json(removePost)
+    const removePost = await Post.findByIdAndDelete({ _id: id });
+    res.status(200).json(removePost);
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(error);
   }
-})
+});
+
+//EDIT POSTS
+router.put(
+  "/editor/:id",
+  authMiddleware,
+  upload.single("thumbnail"),
+  async (req, res) => {
+    const { id } = req.params;
+    const { title, description, content, isPrivate } = req.body;
+    try {
+      const post = await Post.findById(id);
+
+      if (!post) {
+        return res.status(404).json({ message: "Post not found" });
+      }
+
+      //CHECKING EXISTING USER IS THE AUTHOR OF POST
+      if (post.userId.toString() !== req.user.id) {
+        return res
+          .status(403)
+          .json({ message: "Unauthorized to edit this post" });
+      }
+
+      post.title = title || post.title;
+      post.description = description || post.description;
+      post.content = content || post.content;
+      post.isPrivate = isPrivate ?? post.isPrivate;
+
+      //IF NEW UPLOAD CHANGE IT
+      if (req.file) {
+        post.thumbnail = `uploads/${req.file.filename}`;
+      }
+
+      await post.save();
+
+      res.status(200).json({ message: "Post updated successfully", post });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Server error while updating post" });
+    }
+  }
+);
 
 module.exports = router;
