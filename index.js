bcefdba14b064c3a3b293983bc6cd2810b4f4d49
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2025-03-03 11:42:54 +0530

    Multer updates

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const app = express();
 
 //MIDDLEWARE
 app.use(cors());
-app.use(bodyParser.json());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")))
+app.use(express.json());
+app.use("/uploads", express.static('./uploads'))
 
 //ROUTES
 app.use("/api/auth", authRoutes);
--- middleware/multerConfig.js
+++ middleware/multerConfig.js
@@ -4,7 +4,7 @@ const path = require("path");
 //SET STORAGE
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); //FILES STORAGE PATH
+    cb(null, "./uploads"); //FILES STORAGE PATH
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`); //FILENAME
